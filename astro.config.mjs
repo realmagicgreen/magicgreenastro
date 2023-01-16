45fe046164e36746c8a0b00ceee9b93682d228d3
toybreaker <toybreaker@users.noreply.github.com>
2023-01-16 20:57:41 +0800

    get site data from json now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
+import yaml from "@rollup/plugin-yaml"; //maybe needed, currently using .json
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    //maybe needed, not yet used!
+    plugins: [yaml()],
+  },
   site: "https://magicgreen.junglestar.org",
+  prova: "bomba",
   outDir: "./dist",
 });
