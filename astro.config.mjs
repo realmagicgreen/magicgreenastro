22d2abe9b47db3b3e5ea3dd2ff54d5d683819363
toybreaker <toybreaker@users.noreply.github.com>
2023-06-07 15:07:56 +0800

    fix breaking changes > astro 2.6.0 upg | v6.11.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,19 @@ import prefetch from '@astrojs/prefetch'
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
-    inlineStylesheets: `always`
+    assets: true
   },
   output: 'static',
+  build: {
+    // The 'auto' option will inline only the stylesheets that are below the vite.build.assetsInlineLimit threshold,
+    // reducing the number of requests for smaller sheets.
+    // Larger stylesheets, such as global ones used by all pages,
+    // will still be fetched externally and cached:
+    // inlineStylesheets: 'auto'
+
+    // inline all stylesheets:
+    inlineStylesheets: 'always'
+  },
   markdown: {
     drafts: true,
     rehypePlugins: [rehypeHeadingIds],
